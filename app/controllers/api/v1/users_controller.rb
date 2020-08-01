93d04cf8d1680113728899ffa3f9b3d29126587d
github username <github email address>
2020-08-01 14:18:22 -0400

    Added action controller create and show, added before_action and skip_before_actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,46 @@
 class Api::V1::UsersController < ApplicationController
 
-    def create
-        user = User.create(user_params)
-        if user.save
-            render json: user
-        else 
-            render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+    skip_before_action :authenticate, only: [:create]
+    before_action :set_user, only: [:show, :update]
+    before_action :authorize_user, only: [:update]
 
+def create
+    @user = User.create(user_params)
+  
+    if @user.valid?
+      jwt = Auth.issue({user_id: @user.id})
+      byebug
+      render json: {user: UserSerializer.new(@user), jwt: jwt}, status: :created
+      
+    else
+      render json: {error: "Invalid Email or Password!"}, status: :not_acceptable
+    end
+end
 
-    private 
+    def show
+      render json: @user 
+    end
+    def profile
+      @user = current_user
+      render json: @user, status: 200
+    end
+    def update 
+      if @user.update(user_params)
+        render json: @user, status: 200
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def authorize_user
+      render json: {}, status: :forbidden unless current_user == @user
+    end
     
     def user_params
         params.require(:user).permit(:name,
@@ -19,8 +50,4 @@ class Api::V1::UsersController < ApplicationController
                                     )
     end
 
-
-
-    
- 
 end
