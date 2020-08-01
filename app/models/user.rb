815bc9d2b6c2ae39378b1e66c442f66910297463
github username <github email address>
2020-08-01 14:14:20 -0400

    Seperated relationships, validations, and has_secure_password for a better dev ux

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
 
 
     has_secure_password
+
     validates :name, length: { minimum: 2 }
     validates :user_email, :family_member_email, presence: true
     validates :user_email, uniqueness: true
     validates :password, length: { in: 4..64 }
+
 
 end
